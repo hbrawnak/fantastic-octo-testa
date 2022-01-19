1c5e1ed4ef44f3c360aa682102ad93d5d3c69c37
Habib <habib@green-red.com>
2022-01-19 12:54:12 +0600

    getList passed the test

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -2,21 +2,22 @@
 
 namespace Tests\Feature;
 
+use App\Models\TodoList;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TodoListTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
+    use RefreshDatabase;
+
     public function test_get_todo_list()
     {
+        TodoList::create(['name' => 'Todo list']);
+
         $response = $this->getJson(route('todo-list.store'));
 
+       // dd($response->json());
         $this->assertEquals(1, count($response->json()));
     }
 }
