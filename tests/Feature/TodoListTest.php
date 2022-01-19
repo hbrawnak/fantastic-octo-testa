c7b8c0096e3216b3d2334c7e7fd67cc9a8a79f67
Habib <habib@green-red.com>
2022-01-19 13:07:26 +0600

    Created factory for todolist

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -13,11 +13,12 @@ class TodoListTest extends TestCase
 
     public function test_get_todo_list()
     {
-        TodoList::create(['name' => 'Todo list']);
+        TodoList::factory()->create(['name' => 'todo-list']);
 
         $response = $this->getJson(route('todo-list.store'));
 
-       // dd($response->json());
+        //dd($response->json());
         $this->assertEquals(1, count($response->json()));
+        $this->assertEquals('todo-list', $response->json()[0]['name']);
     }
 }
